c8037996166e98a584b39fb16fbab0a4621083ef
dean <dean@benbenguo.cn>
2021-04-26 15:29:44 +0800

    nio FileExistTutorial.java

---
+++ src/main/java/org/dean/java/tutorial/nio/file/FileExistTutorial.java
@@ -0,0 +1,12 @@
+package org.dean.java.tutorial.nio.file;
+
+public class FileExistTutorial {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean exist() {
+        return false;
+    }
+}
---
+++ src/main/java/org/dean/java/tutorial/nio/package-info.java
@@ -0,0 +1,4 @@
+/**
+ *
+ */
+package org.dean.java.tutorial.nio;
